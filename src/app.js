d3ec9e03c73584864e81f2ff955fda9859fed993
Ruta-B <baumaneruta38@gmail.com>
2024-08-11 22:44:03 +0200

    adjust id , js

--- src/app.js
+++ src/app.js
@@ -28,3 +28,24 @@ let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
 
 currentDateELement.innerHTML = formatDate(currentDate);
+
+function getWeather(response) {
+  let currentTemp = document.querySelector("#current-temperature");
+  let cityElement = document.querySelector("#current-city");
+  let temperature = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  currentTemp.innerHTML = temperature;
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#location-city");
+
+  let city = searchInputElement.value;
+  let apiKey = "a7oe402d391da40bfcfe02337et07b50";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(getWeather);
+}
+
+let searchForm = document.querySelector("#submit-form");
+searchForm.addEventListener("submit", search);
