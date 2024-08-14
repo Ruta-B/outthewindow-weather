a1adb11186a69808f5daceb2ae2c15f2997c2946
Ruta-B <baumaneruta38@gmail.com>
2024-08-14 16:30:21 +0200

    add humidity value

--- src/app.js
+++ src/app.js
@@ -35,6 +35,8 @@ function getWeather(response) {
   let cityElement = document.querySelector("#current-city");
   let temperature = Math.round(response.data.temperature.current);
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity-value");
+  humidityElement.innerHTML = `${response.data.temperature.humidity} %`;
   descriptionElement.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
   currentTemp.innerHTML = temperature;
